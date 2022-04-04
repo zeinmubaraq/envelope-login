a0ada0dc3c3b6a5d06acbbd7df5bcedd6d31945e
zeinmubaraq <zainmubaraq@gmail.com>
2022-04-04 13:03:28 +0700

    make introducing side

--- pages/login.tsx
+++ pages/login.tsx
@@ -11,10 +11,18 @@ const Login: NextPage = () => {
 
       <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
         <div className="flex w-2/3 max-w-4xl rounded-xl bg-white shadow-xl">
-          <div className="w-2/5 rounded-l-xl bg-envelope-blue p-5 text-white">
-            <p>Left side</p>
+          <div className="relative w-1/2 rounded-l-xl bg-envelope-blue text-white">
+            <span className="text-md absolute top-7 left-14 font-bold">Envelope</span>
+            <div className="px-14 py-36">
+              <h2 className="mb-3 text-3xl font-bold">Introducing our 2020 report</h2>
+              <p className="mb-3 text-xs">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit, sed do eiusmod tempor
+                incididun.
+              </p>
+              <button className="rounded-md border border-white px-14 py-2">View Report</button>
+            </div>
           </div>
-          <div className="w-3/5">
+          <div className="w-1/2">
             <p>Right side</p>
           </div>
         </div>
