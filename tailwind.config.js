6d9a7a8224d8bba2fbd77b73a06bc12f23878b58
zeinmubaraq <zainmubaraq@gmail.com>
2022-04-04 12:17:37 +0700

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        'envelope-blue': '#2c6cb0',
+      },
+    },
+  },
+  plugins: [],
+};
